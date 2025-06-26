30d95801d09007cdfc6858fd7550d6e39b7e4fef
Usmonov Shohruxmirzo <usmonovshohruxmirzo@gmail.com>
2025-06-26 18:32:45 +0500

    feat: add scoped CSS and static web assets for Student Management System
    - Introduced scoped CSS for MainLayout and NavMenu components, enhancing styling and layout.
    - Updated static web assets manifest to include new CSS files and their properties.
    - Added cache control and integrity checks for improved performance and security.
    - Included favicon and bootstrap assets in the project structure.

--- StudentManagementSystem/obj/Debug/net8.0/StudentManagementSystem.AssemblyInfo.cs
+++ StudentManagementSystem/obj/Debug/net8.0/StudentManagementSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StudentManagementSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e61bc498855763c61bb8051797f668fd7fd672cb")]
 [assembly: System.Reflection.AssemblyProductAttribute("StudentManagementSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StudentManagementSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

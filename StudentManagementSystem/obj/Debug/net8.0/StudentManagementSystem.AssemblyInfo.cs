329be942136bfa8164a11b5f19c4e87d34584c28
Usmonov Shohruxmirzo <usmonovshohruxmirzo@gmail.com>
2025-06-27 19:58:26 +0500

    refactor: add Font Awesome and improve sidebar functionality
    - Added Font Awesome stylesheet link in App.razor.
    - Updated MainLayout.razor to accept isSibebarOpen parameter and added a toggle button for the sidebar.
    - Implemented ToggleSidebar method to manage sidebar visibility.
    - Modified CSS for MainLayout and Sidebar for improved layout and display behavior.
    - Updated sidebar menu items to use Font Awesome icons.
    - Simplified Home.razor to display a single heading.
    - Updated project file for new debugger flavor and refreshed static web assets manifest.
    - Updated assembly information and cache files to reflect recent changes.

--- StudentManagementSystem/obj/Debug/net8.0/StudentManagementSystem.AssemblyInfo.cs
+++ StudentManagementSystem/obj/Debug/net8.0/StudentManagementSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StudentManagementSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8d335a09fb4bc277a14f1d862ae43c9f8430882")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+640076b2ebee7c75b5048f5b0edd16cf66ec7e9d")]
 [assembly: System.Reflection.AssemblyProductAttribute("StudentManagementSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StudentManagementSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

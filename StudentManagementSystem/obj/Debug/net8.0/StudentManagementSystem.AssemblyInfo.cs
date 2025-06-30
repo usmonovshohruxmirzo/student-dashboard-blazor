e9a075a2fca5396aaae7dff1bf654d0660cf95f8
Usmonov Shohruxmirzo <usmonovshohruxmirzo@gmail.com>
2025-06-30 17:27:56 +0500

    refactor: added reposrts page

--- StudentManagementSystem/obj/Debug/net8.0/StudentManagementSystem.AssemblyInfo.cs
+++ StudentManagementSystem/obj/Debug/net8.0/StudentManagementSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StudentManagementSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+114c2154d21390a8e9f5533283d09ffa8d116eef")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6e65398a865a7e44d7601f9f639737f4ac6d1f2")]
 [assembly: System.Reflection.AssemblyProductAttribute("StudentManagementSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StudentManagementSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

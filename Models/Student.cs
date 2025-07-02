07b0cb6b362db3ed4f666eda2d36c239b81cb83d
Usmonov Shohruxmirzo <usmonovshohruxmirzo@gmail.com>
2025-07-02 19:10:31 +0500

    feat: added Student.cs & StudentService.cs

---
+++ Models/Student.cs
@@ -0,0 +1,13 @@
+﻿namespace NET9.BlazorWebAppServerGlobal.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; } = string.Empty;
+        public string Gender { get; set; } = string.Empty;
+        public DateTime DateOfBirth { get; set; }
+        public double Gpa { get; set; }
+        public List<string> Subjects { get; set; } = new();
+        public string Status { get; set; } = "Active";
+    }
+}
---
+++ Services/StudentService.cs
@@ -0,0 +1,6 @@
+﻿namespace NET9.BlazorWebAppServerGlobal.Services;
+
+public class StudentService
+{
+    
+}

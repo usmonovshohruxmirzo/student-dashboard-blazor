b27c9185633670e944f2c5b338b8aebaf0bb653b
Shohruhmirzo Usmonov <usmonovshohruxmirzo@gmail.com>
2026-06-04 18:39:32 +0500

    refactor(dtos): add data annotations

--- src/Application/DTOs/CreateStudentDTO.cs
+++ src/Application/DTOs/CreateStudentDTO.cs
@@ -1,9 +1,17 @@
+using System.ComponentModel.DataAnnotations;
 using StudentManagement.Domain.Enums;
 
-namespace StudentManagement.Application.DTOs;
+public class CreateStudentDto
+{
+    [Required(ErrorMessage = "Name is required")]
+    public string Name { get; set; } = "";
 
-public sealed record CreateStudentDto(
-    string Name,
-    Gender Gender,
-    decimal Gpa,
-    StudentStatus Status);
+    [Required(ErrorMessage = "Gender is required")]
+    public Gender? Gender { get; set; }
+
+    [Range(0, 4, ErrorMessage = "GPA must be between 0 and 4")]
+    public decimal Gpa { get; set; }
+
+    [Required(ErrorMessage = "Status is required")]
+    public StudentStatus? Status { get; set; }
+}

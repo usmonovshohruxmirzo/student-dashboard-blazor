55f419dae49cc8669e0c31322545e99a88777950
Shohruhmirzo Usmonov <usmonovshohruxmirzo@gmail.com>
2026-06-04 18:42:49 +0500

    feat: update service registrations and configure Swagger for development

--- src/Program.cs
+++ src/Program.cs
@@ -1,21 +1,32 @@
 using Microsoft.AspNetCore.Components;
-using NET9.BlazorWebAppServerGlobal.Presentation;
-using NET9.BlazorWebAppServerGlobal.Services;
+using Microsoft.EntityFrameworkCore;
+using StudentManagement.Domain.Entities;
+using StudentManagement.Domain.Interfaces;
+using StudentManagement.Infrastructure.Persistence;
+using StudentManagement.Infrastructure.Repositories;
+using StudentManagement.Presentation;
+using StudentManagement.Application.Services;
+using StudentManagement.Application.Interfaces;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddSingleton<StudentService>();
+builder.Services.AddScoped<IStudentService, StudentService>();
 
 builder.Services.AddControllers();
 
 builder.Services.AddHttpClient("ApiClient", client =>
 {
-  client.BaseAddress = new Uri("https://localhost:5001/");
+  client.BaseAddress = new Uri("https://localhost:5126/");
 });
 
 builder.Services.AddScoped(sp => new HttpClient
 {
   BaseAddress = new Uri(sp.GetRequiredService<NavigationManager>().BaseUri)
+});
+builder.Services.AddScoped<IRepository<Student, int>, EfRepository<Student, int>>();
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+  options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
 builder.Services.AddRazorComponents()
@@ -23,12 +34,20 @@ builder.Services.AddRazorComponents()
 
 builder.Services.AddBlazorBootstrap();
 
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
 if (!app.Environment.IsDevelopment())
 {
   app.UseExceptionHandler("/Error", createScopeForErrors: true);
   app.UseHsts();
+}
+
+if (app.Environment.IsDevelopment())
+{
+  app.UseSwagger();
+  app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
